c23c680033d027025649e71e5bb9d095a99c1bb4
Lauro Torster <laurotorster_rg@hotmail.com>
2022-11-04 18:34:02 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function createGame(player1, hour, player2) {
+    return `
+        <li>
+            <img src="./assets/incon-${player1}.svg" alt="Bandeira do ${player1}">
+            <strong>${hour}</strong>
+            <img src="./assets/incon-${player2}.svg" alt="Bandeira do ${player2}">
+        </li>
+    `
+}
+
+let delay = -0.2;
+function createCard(date, day, games) {
+   delay = delay + 0.2;
+    return `
+        <div class="card" style="animation-delay: ${delay}s">
+            <h2>${date} <span>${day}</span></h2>
+
+            <ul>
+               ${games}
+            </ul>
+        </div>
+    `
+}
+
+document.querySelector('#cards').innerHTML =
+            createCard("24/11", "Quinta Feira", 
+            createGame("brazil", "16:00", "serbia")) +
+            createCard("28/11", "Segunda Feira", 
+            createGame("brazil", "13:00", "switzerland")) +
+            createCard("02/12", "Sexta Feira", 
+            createGame("brazil", "16:00", "cameroon"))
